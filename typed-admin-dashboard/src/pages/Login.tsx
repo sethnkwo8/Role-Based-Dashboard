64e5a74d2c8ff22b25739ea591f7c5e0b15e6f5c
Seth Nkwo <sethnkwocool@gmail.com>
2026-02-05 18:10:19 +0100

    Created Login Page and functionalities

---
+++ typed-admin-dashboard/src/pages/Login.tsx
@@ -0,0 +1,46 @@
+import { useState } from "react"
+import { useAuth } from "../auth/useAuth"
+import type { Role } from "../auth/auth.types";
+
+export function Login() {
+
+    const { login } = useAuth()
+
+    const [username, setUsername] = useState<string>('');
+    const [role, setRole] = useState<Role | ''>('')
+
+
+    function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+        setUsername(e.target.value)
+    }
+
+    function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+        e.preventDefault()
+
+        if (role !== '') {
+            login(username, role)
+        }
+    }
+
+    function handleSelectChange(e: React.ChangeEvent<HTMLSelectElement>) {
+        const value = e.target.value
+        setRole(value as Role)
+    }
+
+    return (
+        <div className="min-h-screen flex flex-row justify-center items-center">
+            <form className="rounded-lg shadow-lg p-4 flex flex-col items-center space-y-4 w-1/2" onSubmit={handleSubmit}>
+                <h1 className="font-bold text-4xl">Login</h1>
+                <label className="font-bold text-2xl" htmlFor="username">Username</label>
+                <input className="w-full outline-0 rounded-lg border border-black p-2" id="username" onChange={handleChange} required type="text" value={username} />
+                <select id="roleSelect" onChange={handleSelectChange} required value={role}>
+                    <option value="">Select a role</option>
+                    <option value="Admin">Admin</option>
+                    <option value="User">User</option>
+                    <option value="Staff">Staff</option>
+                </select>
+                <button className="rounded-lg py-4 px-8 bg-blue-400 hover:bg-blue-600 text-white">Login</button>
+            </form>
+        </div>
+    )
+}
